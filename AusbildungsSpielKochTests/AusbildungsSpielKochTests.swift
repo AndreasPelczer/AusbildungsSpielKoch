000aba7739b1c50c3fa99c1136b030b65382736f
Andreas Pelczer <134264285+AndreasPelczer@users.noreply.github.com>
2026-02-15 12:12:53 +0100

    Add files via upload
    los it goes

---
+++ AusbildungsSpielKochTests/AusbildungsSpielKochTests.swift
@@ -0,0 +1,17 @@
+//
+//  AusbildungsSpielKochTests.swift
+//  AusbildungsSpielKochTests
+//
+//  Created by Andreas Pelczer on 15.02.26.
+//
+
+import Testing
+@testable import AusbildungsSpielKoch
+
+struct AusbildungsSpielKochTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
